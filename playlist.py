2cd49cd37bb33f46173fee5879fce6dbb3df71a9
Jared Grayston <jjgrayston@users.noreply.github.com>
2018-08-31 22:56:40 +1000

    Update playlist.py
    Updated to use V3 of Youtube API

--- playlist.py
+++ playlist.py
@@ -1,5 +1,6 @@
 import urllib, json,sys
 import subprocess
+import os
 
 def play_url(url):
     yt_dl = subprocess.Popen(['youtube-dl', '--max-quality' , '18', '-g', url], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
@@ -7,32 +8,38 @@ def play_url(url):
     if yt_dl.returncode != 0:
         sys.stderr.write(err)
         raise RuntimeError('Error getting URL.')
-	print "Playing"
+        print "Playing"
     mplayer = subprocess.Popen(
             ['omxplayer','-b', url.decode('UTF-8').strip()],
             stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     mplayer.wait()
-	
+
 
 input = sys.argv
-keyword = input[1]
+playlist = input[1]
+
+key = os.environ['YOUTUBE_API_KEY']
 
 print 'Play List:', str(input[1])
- 
-url = "https://gdata.youtube.com/feeds/api/playlists/"+keyword+"?alt=json"
+
+url = "https://www.googleapis.com/youtube/v3/playlistItems?playlistId="+playlist+"&maxResults=50&part=snippet%2CcontentDetails&key="+key
+
 response = urllib.urlopen(url);
 data = json.loads(response.read())
-feed = data['feed']['entry']
+feed = data['items']
 
 print 'Found ',len(feed),' Video'
 
 for d in feed:
-	jsonstring = json.dumps(d)
-	title = d['title']['$t']
-	link = d['link'][0]['href']
-	print "Play : ",title
-	play_url(link)
-	print "DONE"
+        jsonstring = json.dumps(d)
+        title = d['snippet']['title']
+        link = "https://www.youtube.com/watch?v=" + d['contentDetails']['videoId']
+        print "Play : ",title
+        play_url(link)
+        print "DONE"
+
+#if 'nextPageToken' in data:
+#    print "Another page is available."
+#else:
+#    print "That was the last page."
 print "############# Play All Done ################"
-
-
